0d184baab99b9e192a0f5a57b58905812fc51a98
Subash Chandran <SubashChandran.Thirumaran@team.neustar>
2018-02-26 20:15:44 +0530

    Add Router Configuration

--- angular-5-client/src/app/app.module.ts
+++ angular-5-client/src/app/app.module.ts
@@ -1,31 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
-import { RegisterComponentComponent } from './register-component/register-component.component';
-import { CarFindComponentComponent } from './car-find-component/car-find-component.component';
 import { CarFindComponent } from './car-find/car-find.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
+const appRoutes: Routes = [
+  {
+    path: '', component: CarFindComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponentComponent,
-    RegisterComponentComponent,
-    CarFindComponentComponent,
     CarFindComponent,
     LoginComponent,
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
